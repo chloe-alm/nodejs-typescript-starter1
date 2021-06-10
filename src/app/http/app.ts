d3798e574f077fa23ed113fdeab6d475f3620859
MacBook-Pro-de-Chloe <cdargelez@gmail.com>
2021-06-10 20:31:57 +0200

    add cors

--- src/app/http/app.ts
+++ src/app/http/app.ts
@@ -6,13 +6,13 @@ import { API_BASE_URL } from "../../constant";
 //Documentation
 import swaggerConfig from "../../middlewares/documentation/swagger.json";
 import swaggerUi from "swagger-ui-express";
-
+import cors from 'cors';
 //Logger
 import morgan from "morgan";
 
 export const createServer = async (): Promise<express.Application> => {
   const app: express.Application = express();
-
+app.use(cors())
   app.use(express.json());
   app.use(morgan("dev"));
 
